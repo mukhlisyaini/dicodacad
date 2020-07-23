3f21fd4d71b3ad37ead00630e3ccd2d9cc5eed4c
mukhlisyaini <mukhlisyaini@gmail.com>
2020-07-23 15:47:08 +0800

    mulai bahas kalkulator dan javascript

---
+++ jsscript/kalkulator.js
@@ -0,0 +1,8 @@
+/* Ini merupakan komentar dengan lebih dari satu baris
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
+
+//contoh komentar
+
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
